5312ef4aa6ed388177610cdd5c252e3c0897dbb3
sir-aphim <123579499+sir-aphim@users.noreply.github.com>
2024-05-13 16:51:58 -0300

    Develop create card function to handle responsive elements

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const titleInputField = document.getElementById('title')
 const authorInputField = document.getElementById('author')
 const pagesInputField = document.getElementById('pages')
 const releaseInputField = document.getElementById('release')
+const readStatus = document.getElementById('read')
 const newBook = document.querySelector('.balloon');
 const uploadBook = document.querySelector('input.balloon[type="submit"]');
 const closeFormModal = document.querySelector('button.close')
@@ -11,21 +12,23 @@ let title = titleInputField.value;
 let author = authorInputField.value;
 let pages = pagesInputField.value;
 let release = releaseInputField.value;
+let read = readStatus.value;
 
 const myLibrary = [];
 
-function Book(title, author, release, pages) {
+function Book(title, author, release, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    // this.read = read;
+    this.release = release;
+    this.read = read;
     this.info = function () {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`
     }
 }
 
 function addBookToLibrary() {
-    myLibrary.push(title, author, pages, release)
+    myLibrary.push(title, author, pages, release, read)
 }
 
 newBook.addEventListener("click", () => {
@@ -33,19 +36,43 @@ newBook.addEventListener("click", () => {
     dialog.showModal();
 });
 
-uploadBook.addEventListener("click", () => {
-    event.preventDefault();
-    addBookToLibrary();
-    console.log(title, author, pages, release)
-    console.log(myLibrary)
-    dialog.close();
-})
-
 closeFormModal.addEventListener("click", () => {
     event.preventDefault();
     dialog.close();
     console.log('x')
 });
+
+function createCard() {
+    let newCard = document.createElement("div");
+    let mainCard = document.createElement("div")
+    let titlePara = document.createElement("p")
+    let infoPara = document.createElement("p")
+    let cardsDiv = document.querySelector(".cards");
+
+    titlePara.textContent = `${title}, by ${author} (${release})`;
+    infoPara.textContent = `${pages} Pages`;
+
+    newCard.classList.add("card");
+    titlePara.classList.add("title");
+    infoPara.classList.add("info")
+    mainCard.classList.add("main");
+
+    newCard.appendChild(mainCard)
+    cardsDiv.appendChild(newCard);
+    mainCard.appendChild(titlePara)
+    mainCard.appendChild(infoPara)
+  };
+
+uploadBook.addEventListener("click", () => {
+    event.preventDefault();
+    addBookToLibrary();
+    createCard();
+    console.log(title, author, pages, release, read)
+    console.log(myLibrary)
+    dialog.close();
+});
+
+
 
 myLibrary.forEach(element => {
     console.log(element)
