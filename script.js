3a5fe1cb6aa9aad14048bc07b5d8c07c34b6329f
sir-aphim <123579499+sir-aphim@users.noreply.github.com>
2024-05-27 14:50:30 -0300

    Add read if conditional

--- script.js
+++ script.js
@@ -54,7 +54,8 @@ function createCard() {
     const starPlusSVG = createSVG("project-option", "0 0 24 24", '<title>star-plus-outline</title><path d="M5.8 21L7.4 14L2 9.2L9.2 8.6L12 2L14.8 8.6L22 9.2L18.8 12H18C17.3 12 16.6 12.1 15.9 12.4L18.1 10.5L13.7 10.1L12 6.1L10.3 10.1L5.9 10.5L9.2 13.4L8.2 17.7L12 15.4L12.5 15.7C12.3 16.2 12.1 16.8 12.1 17.3L5.8 21M17 14V17H14V19H17V22H19V19H22V17H19V14H17Z" />');
     const trashCanSVG = createSVG("project-option", "0 0 24 24", '<title>trash-can-outline</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />');
     const eyePlusSVG = createSVG("project-option", "0 0 24 24", '<title>eye-plus-outline</title><path d="M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C12.36,19.5 12.72,19.5 13.08,19.45C13.03,19.13 13,18.82 13,18.5C13,18.14 13.04,17.78 13.1,17.42C12.74,17.46 12.37,17.5 12,17.5C8.24,17.5 4.83,15.36 3.18,12C4.83,8.64 8.24,6.5 12,6.5C15.76,6.5 19.17,8.64 20.82,12C20.7,12.24 20.56,12.45 20.43,12.68C21.09,12.84 21.72,13.11 22.29,13.5C22.56,13 22.8,12.5 23,12C21.27,7.61 17,4.5 12,4.5M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M18,14.5V17.5H15V19.5H18V22.5H20V19.5H23V17.5H20V14.5H18Z" />');
-   
+    const eyeCheckSVG = createSVG("project-option", "0 0 24 24", '<title>eye-check</title><path d="M23.5,17L18.5,22L15,18.5L16.5,17L18.5,19L22,15.5L23.5,17M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,17C12.5,17 12.97,16.93 13.42,16.79C13.15,17.5 13,18.22 13,19V19.45L12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5C17,4.5 21.27,7.61 23,12C22.75,12.64 22.44,13.26 22.08,13.85C21.18,13.31 20.12,13 19,13C18.22,13 17.5,13.15 16.79,13.42C16.93,12.97 17,12.5 17,12A5,5 0 0,0 12,7A5,5 0 0,0 7,12A5,5 0 0,0 12,17Z" />');
+
     trashCanSVG.addEventListener("click", function() { // Add event listener to remove button
         removeBook(title, author, release, pages, read);
         fadeItem(newCard)
@@ -72,13 +73,18 @@ function createCard() {
 
     projectOptions.appendChild(starPlusSVG); // Append star plus SVG to project options
     projectOptions.appendChild(trashCanSVG)
-    projectOptions.appendChild(eyePlusSVG);
+
+    if (read === "Read") {
+        projectOptions.appendChild(eyeCheckSVG);
+    } else {
+        projectOptions.appendChild(eyePlusSVG);
+    };
 
     newCard.appendChild(mainCard)
     cardsDiv.appendChild(newCard);
-    mainCard.appendChild(titlePara)
-    mainCard.appendChild(infoPara)
-    newCard.appendChild(projectOptions)
+    mainCard.appendChild(titlePara);
+    mainCard.appendChild(infoPara);
+    newCard.appendChild(projectOptions);
   };
 
 function createSVG(className, viewBox, pathData) {
